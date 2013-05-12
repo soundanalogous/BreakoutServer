be9e5b321cd4fbea616a8bd5bb562c6ccd6c8de3
Jeff Hoefs <jeff.hoefs@gmail.com>
2013-05-11 22:05:40 -0400

    updated build.bat and config.xml files for Win builds

--- resources/build.bat
+++ resources/build.bat
@@ -1,1 +1,1 @@
-exewrap.exe -g -i BreakoutLogo.ico -o "Breakout Server32.exe" -v 0.2.0.0 BreakoutServer.jar
+exewrap.exe -g -i BreakoutLogo.ico -o "Breakout Server32.exe" -v 0.2.3.0 BreakoutServer.jar
